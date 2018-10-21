eda0c893cbf4a76af71074987203806536b92ccb
Julio Zebadua <julio.zebadua@schibsted.com>
2018-10-20 23:27:45 -0500

    basic R implements response

--- response.go
+++ response.go
@@ -11,14 +11,19 @@ type Response interface {
 	SetStatus(status string)
 }
 
-// Writer is a func that can write a response to writer
+// Writer is a func that can write a response to writer.
 type Writer func(w io.Writer, r Response)
 
-// R basic response for json
+// R basic response for json.
 type R struct {
 	Status string      `json:"status"`
 	Data   interface{} `json:"data,omitempty"`
-	Err    error       `json:"error,omitempty"`
+	Error  error       `json:"error,omitempty"`
+}
+
+// SetStatus implemented response interface.
+func (r *R) SetStatus(status string) {
+	r.Status = status
 }
 
 // JSON a response to a writer.
@@ -41,7 +46,7 @@ func OK(w io.Writer, r Response, wf Writer) {
 	wf(w, r)
 }
 
-// Error set error status to response to a writer
+// Error set error status to response to a writer.
 func Error(w io.Writer, r Response, wf Writer) {
 	r.SetStatus("error")
 	if hw, ok := w.(http.ResponseWriter); ok {
@@ -52,7 +57,7 @@ func Error(w io.Writer, r Response, wf Writer) {
 	wf(w, r)
 }
 
-// Deny write the deny response
+// Deny write the deny response.
 func Deny(w io.Writer, r Response, wf Writer) {
 	r.SetStatus("deny")
 	if hw, ok := w.(http.ResponseWriter); ok {
